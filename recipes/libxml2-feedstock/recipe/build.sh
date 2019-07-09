54bfdc7bdfd472f8a700e07a7e1146ba03f63b74
Jonathan Helmus <jjhelmus@gmail.com>
2019-07-09 04:44:28 +0000

    libxml2: remove icu and libiconv reqs
    libxml2 should not depend on libiconv on linux where it is included in
    glibc.  Removing icu makes for a smaller package.

--- recipes/libxml2-feedstock/recipe/build.sh
+++ recipes/libxml2-feedstock/recipe/build.sh
@@ -7,7 +7,6 @@
             --host=$HOST \
             --with-iconv="${PREFIX}" \
             --with-zlib="${PREFIX}" \
-            --with-icu \
             --with-lzma="${PREFIX}" \
             --without-python
 make -j${CPU_COUNT} ${VERBOSE_AT}
